858cfc8e7cb99715590eab5924c5ee1e74c48008
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2021-10-27 14:32:09 -0400

    adding example data

--- examples/read_dump.jl
+++ examples/read_dump.jl
@@ -4,7 +4,7 @@ include("../src/packer.jl")
 include("../src/unpacker.jl")
 
 
-stream_from_dump = read_stream(load_stream("multiple.hex"))
+stream_from_dump = read_stream(load_stream("../data/multiple.hex"))
 
 println("Hit matrix of event 1 after hex dump:")
 get_hit_matrix(stream_from_dump[1])
