1d27e47385c24ec64e9280cfb6b888dde704dd5d
Chris McMahon <cjm01@bu.edu>
2021-11-11 15:11:16 -0500

    add 2d plot for TOT

--- examples/read_dump.jl
+++ examples/read_dump.jl
@@ -2,6 +2,7 @@ include("../src/common.jl")
 include("../src/utils.jl")
 include("../src/packer.jl")
 include("../src/unpacker.jl")
+include("../src/plotting.jl")
 
 
 stream_from_dump = read_stream(load_stream("../data/multiple.hex"))
@@ -11,3 +12,4 @@ println("Hit matrix of event 1 after hex dump:")
 # print("number of hits: ", length(get_pixel_hits(stream_from_dump[1])), "\n")
 #get_hit_matrix(stream_from_dump[1])
 print(stream_from_dump[1])
+plot_evt(stream_from_dump[1])
