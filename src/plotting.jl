563adde7970184e53d86ad22ada094d62393cdfd
Chris McMahon <cjm01@bu.edu>
2021-11-17 16:34:40 -0500

    add plotting.jl

---
+++ src/plotting.jl
@@ -0,0 +1,19 @@
+using Plots
+include("utils.jl")
+include("common.jl")
+include("packer.jl")
+
+function plot(event::Event, type::String="TOT")
+    data = transpose(get_matrix(event, type))
+    heatmap([1:size(data, 1)], [1:size(data,2)], data, size=(700,700),
+            xticks=1:16, yticks=1:16, title=type, xlabel="Column", ylabel="Row") 
+end
+
+function plot(stream::Vector{Event})
+    hits = zeros(16,16)
+    for evt in stream:
+        hits = hits + get_matrix(evt)
+    end
+    heatmap([1:size(hits, 1)], [1:size(hits,2)], hits, size=(700,700),
+            xticks=1:16, yticks=1:16, title="Hits in Each Pixel", xlabel="Column", ylabel="Row") 
+end
