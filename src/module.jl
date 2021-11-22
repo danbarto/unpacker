472ff92f23b20f0ab2390ee47932779cdff0d859
Chris McMahon <cjm01@bu.edu>
2021-11-22 11:25:42 -0500

    add FIFO, KCU, ReadoutBoard imports

--- src/module.jl
+++ src/module.jl
@@ -1,6 +1,5 @@
 using PyCall
 #pushfirst!(PyVector(pyimport("sys")["path"]), "") #add current directory to python path
-#pushfirst!(PyVector(pyimport("sys")["path"]), "/home/chris/Projects/ETROC Data Card/unpacker/module_test_sw/tamalero")
 #pushfirst!(PyVector(pyimport("sys")."path"), @__DIR__)
 println(PyVector(pyimport("sys")["path"]))
 np = pyimport("numpy")
@@ -10,5 +9,14 @@ t1 = pyimport("test")
 sys = pyimport("sys")
 
 println(sys.path)
-kcu = pyimport("KCU")
-#test = t1.test_obj()
+kcu = pyimport("tamalero.KCU")
+ReadotBoard = pyimport("tamalero.ReadoutBoard")
+rb_0 = kcu.connect_readout_board(ReadoutBoard(0, trigger=false))
+FIFO = pyimport("tamalero.FIFO")
+fifo = FIFO(rb_0, elink=2)
+fifo.set_trigger(word0=0x35, word1=0x55, mask0=0xff, mask1=0xff)
+fifo.reset()
+hex_dump = fifo.giant_dump(3000,255)
+fifo.dump_to_file(hex_dump, n_col=5, filename ="julia_dump.hex")  # use 5 columns --> better to read for our data format
+
+
